93f520de2f56f20faf1bdd7f16056729bbf1c095
razzaq1140 <162542388+razzaq1140@users.noreply.github.com>
2025-04-11 18:40:56 +0500

    error solve of provider in foodlist provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:loader_overlay/loader_overlay.dart';
+import 'package:smart_restaurant_app/src/features/food_list/pages/provider/food_list_provider.dart';
+import 'package:smart_restaurant_app/src/features/product/provider/product_provider.dart';
 import 'package:smart_restaurant_app/src/router/routes.dart';
 import 'package:smart_restaurant_app/src/theme/theme.dart';
 
@@ -12,13 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GlobalLoaderOverlay(
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.instance.lightTheme,
-        routerDelegate: MyAppRouter.router.routerDelegate,
-        routeInformationParser: MyAppRouter.router.routeInformationParser,
-        routeInformationProvider: MyAppRouter.router.routeInformationProvider,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => FoodListProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+      ],
+      child: GlobalLoaderOverlay(
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.instance.lightTheme,
+          routerDelegate: MyAppRouter.router.routerDelegate,
+          routeInformationParser: MyAppRouter.router.routeInformationParser,
+          routeInformationProvider: MyAppRouter.router.routeInformationProvider,
+        ),
       ),
     );
   }
